1db87f6838b28dfd6729917eff52aa1e4018c0f7
Asmit Adak <asadak@deloitte.com>
2024-04-06 09:32:43 +0530

    commit ecommerce initials

---
+++ Models/CartItem.cs
@@ -0,0 +1,28 @@
+﻿using AutomationFramework.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework.Models
+{
+    public class CartItem
+    {
+        public string ItemString;
+        public string ItemName;
+       
+
+        public CartItem(String ItemString,String ItemName)
+        {
+            this.ItemString = ItemString;
+            this.ItemName = ItemName;
+        }
+        public static CartItem[] Whattosearch()
+        {
+
+            CartItem[] myArray = new CartItem[] { new CartItem("search anything","anything"), new CartItem("search anything2", "anything" ) };
+            return myArray;
+        }
+    }
+}
---
+++ pages/EcommerceHome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomationFramework.Common;
+using AutomationFramework.Models;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutomationFramework.pages
+{
+    public class EcommerceHome:BasePage
+    {
+        IWebDriver driver;
+        By Searchbox = By.XPath("//input[@placeholder='Search']");
+        By Item=By.XPath($"//*[@class='product-thumb']//h4//a[normalize-space(text()) = 'itemname']");
+
+        public EcommerceHome(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+        }
+
+        public void SearchforItem(CartItem item)
+        {
+            
+        }
+        public void AddItemtoCart(CartItem item)
+        {
+
+        }
+
+    }
+}
