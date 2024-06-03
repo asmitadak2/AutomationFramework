d62d0ee1f586d684212597aa4b81204b8dd25eaf
Asmit Adak <asadak@deloitte.com>
2024-06-03 15:26:18 +0530

    json to test data

---
+++ DataReaders/ItemListReader.cs
@@ -0,0 +1,30 @@
+﻿using AutomationFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutomationFramework.DataReaders
+{
+    public class ItemListReader
+    {
+        private readonly string _sampleJsonFilePath;
+
+        public ItemListReader(string _sampleJsonFilePath)
+        {
+            this._sampleJsonFilePath = _sampleJsonFilePath;    
+        }
+        public List<CartItem> ReadCartItemsfromJSON()
+        {
+            using StreamReader reader = new(_sampleJsonFilePath);
+            var json = reader.ReadToEnd();
+            List<CartItem> items = JsonConvert.DeserializeObject<List<CartItem>>(json);
+            return items;
+        }
+    
+    }
+}
--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -1,4 +1,5 @@
-﻿using AutomationFramework.Resources;
+﻿using AutomationFramework.DataReaders;
+using AutomationFramework.Resources;
 using RazorEngine.Compilation.ImpromptuInterface.Dynamic;
 using System;
 using System.Collections.Generic;
@@ -10,23 +11,35 @@ namespace AutomationFramework.Models
 {
     public class CartItem
     {
-        public string ItemString;
-        public string ItemName;
+
+        public string ItemString { get; set; }  = string.Empty;
+        public string ItemName { get; set; } = string.Empty;
+        //public string ItemString;
+        //public string ItemName;
 
 
-        public CartItem(String ItemString, String ItemName)
-        {
-            this.ItemString = ItemString;
-            this.ItemName = ItemName;
-        }
-        public String get_ItemString{get{ return this.ItemString; } }
-        public String get_ItemName { get{ return this.ItemName; } }
+        //public CartItem(String ItemString, String ItemName)
+        //{
+        //    this.ItemString = ItemString;
+        //    this.ItemName = ItemName;
+        //}
+        //public String get_ItemString{get{ return this.ItemString; } }
+        //public String get_ItemName { get{ return this.ItemName; } }
 
+        //public static CartItem[] Whattosearch()
+        //{
+
+        //    CartItem[] myArray = new CartItem[] { new CartItem("iPhone", "iPhone"), new CartItem("Canon EOS 5D", "Canon EOS 5D") };
+        //    return myArray;
+        //}
         public static CartItem[] Whattosearch()
         {
+            ItemListReader reader=new ItemListReader("C:\\Users\\asadak\\source\\repos\\AutomationFramework\\JSONS\\ItemList.json");
+            List<CartItem> cartitemlist=reader.ReadCartItemsfromJSON();
+            CartItem[] items= cartitemlist.ToArray();
+            return items;
 
-            CartItem[] myArray = new CartItem[] { new CartItem("iPhone", "iPhone"), new CartItem("Canon EOS 5D", "Canon EOS 5D") };
-            return myArray;
+
         }
     }
 }
